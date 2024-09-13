1aa37cb7e6d8fd3ea459aaee00e9fe7e3dd609e4
Enrique Almeida Maldonado <ealmeida.cu@gmail.com>
2024-09-13 00:41:48 -0400

    done (#4)

--- apps/business_app/apps.py
+++ apps/business_app/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class EventAppConfig(AppConfig):
+class BusinessAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.business_app"
-    verbose_name = _("Allele Application")
+    verbose_name = _("Business Specific Application")
