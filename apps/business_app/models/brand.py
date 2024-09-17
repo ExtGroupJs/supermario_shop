321a4aa52531f3585e9d2909e1aea59919bfcd24
Drak <ealmeida.cu@gmail.com>
2024-09-17 15:14:15 -0400

    brand logo not necesary

--- apps/business_app/models/brand.py
+++ apps/business_app/models/brand.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Brand(models.Model):
     name = models.CharField(verbose_name="Nombre", unique=True, max_length=25)
-    logo = models.ImageField(verbose_name="logo")
+    logo = models.ImageField(verbose_name="logo", null=True, blank=True)
 
     class Meta:
         verbose_name = "Marca"
