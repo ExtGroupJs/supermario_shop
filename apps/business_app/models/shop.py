5e97902187331660a31aef3d27928e08c1b8fe4a
Drak <ealmeida.cu@gmail.com>
2024-09-17 15:01:39 -0400

    without migrations

--- apps/business_app/models/shop.py
+++ apps/business_app/models/shop.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+from apps import business_app
+
+# from apps.business_app.models.product import Product
+# from apps.business_app.models.shop_products import ShopProducts
+
 
 class Shop(models.Model):
     name = models.CharField(verbose_name="Nombre", unique=True, max_length=200)
     logo = models.ImageField(verbose_name="logo")
     extra_info = models.TextField(
         verbose_name="Información Extra", null=True, blank=True
+    )
+    products = models.ManyToManyField(
+        to="apps.business_app.Product", through="apps.business_app.ShopProducts"
     )
 
     class Meta:
