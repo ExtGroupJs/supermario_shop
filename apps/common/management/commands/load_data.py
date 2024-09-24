7b0a8589debec355733c1f1a5ac0740adbe1bbb8
Enrique Almeida Maldonado <ealmeida.cu@gmail.com>
2024-09-24 00:15:21 -0400

    16 adicionar roles al sistema (#17)
    * changes
    
    * added backend suport
    
    * added load_data command
    
    * logic for role completed

---
+++ apps/common/management/commands/load_data.py
@@ -0,0 +1,25 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from termcolor import colored
+
+
+class Command(BaseCommand):
+    help = "Loads initial fixtures"
+
+    def handle(self, *args, **options):
+        # print(
+        #     colored(
+        #         "There's no fixtures to add yet",
+        #         "red",
+        #         attrs=["blink"],
+        #     )
+        # )
+
+        call_command("loaddata", "auth.group.json")
+        print(
+            colored(
+                "Successfully added group permissions",
+                "green",
+                attrs=["blink"],
+            )
+        )
--- apps/users_app/urls.py
+++ apps/users_app/urls.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
 
 from apps.users_app.views import UserViewSet
+from apps.users_app.views.group import GroupViewSet
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
-# router.register(r"countries", CountryViewSet, basename="countries")
+router.register(r"groups", GroupViewSet, basename="groups")
 
 urlpatterns = []
 
