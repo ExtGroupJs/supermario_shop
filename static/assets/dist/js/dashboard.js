efe7470abd1060c9c4b52f6427a86d56dfe8062b
Duniesky <dunieskydp86@gmail.com>
2024-10-23 16:00:28 -0400

    dashboard2

--- static/assets/dist/js/dashboard.js
+++ static/assets/dist/js/dashboard.js
@@ -7,12 +7,12 @@ const csrfToken = document.cookie
 // url del endpoint principal
 const url = "/business-gestion/dashboard/shop-product-investment/";
 $(document).ready(function () {
-  // Llama a la función para ejecutar la solicitud
+  
 smallboxdata();
-// Llama a la función para ejecutar la solicitud
 smallboxdataLastMonth();
-// Llama a la función para ejecutar la solicitud
 smallboxdataCurrentMonth();
+smallboxdataSellCurrentWeek();
+smallboxdataSellCurrentMonth();
 });
 
 function smallboxdata() {
@@ -109,8 +109,83 @@ function smallboxdataCurrentMonth() {
         });
 }
 
-// Llama a la función para ejecutar la solicitud
-smallboxdataCurrentMonth();
+function smallboxdataSellCurrentWeek() {
+    // Obtener la fecha actual
+    const today = new Date();
+    
+    // Calcular el primer día de la semana actual (domingo)
+    const firstDayOfCurrentWeek = new Date(today);
+    firstDayOfCurrentWeek.setDate(today.getDate() - today.getDay());
+
+    // Calcular el último día de la semana actual (sábado)
+    const lastDayOfCurrentWeek = new Date(today);
+    lastDayOfCurrentWeek.setDate(today.getDate() + (6 - today.getDay()));
+    
+    // Formatear las fechas a YYYY-MM-DD
+    const startDate = firstDayOfCurrentWeek.toISOString().split('T')[0];
+    const endDate = lastDayOfCurrentWeek.toISOString().split('T')[0];
+
+    // Parámetros para la solicitud
+    const params = {
+        "updated_timestamp__gte": startDate,
+        "updated_timestamp__lte": endDate,
+        "frequency": "week"
+        
+    };
+
+    axios.post('/business-gestion/dashboard/shop-product-sells-count/', params)
+        .then(response => {
+            // Obtener el valor de ventas de la respuesta
+            const sellCount =  response.data.result[0] ? response.data.result[0].total : 0;
+            // Modificar el contenido del small-box con el valor de las ventas
+            const smallBox = document.getElementById('sellweek');
+            if (smallBox) {
+                smallBox.textContent = sellCount + " Ventas";
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+function smallboxdataSellCurrentMonth() {
+    // Obtener la fecha actual
+    const today = new Date();
+    
+    // Calcular el primer día del mes actual
+    const firstDayOfCurrentMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    
+    // Calcular el último día del mes actual
+    const lastDayOfCurrentMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+    
+    // Formatear las fechas a YYYY-MM-DD
+    const startDate = firstDayOfCurrentMonth.toISOString().split('T')[0];
+    const endDate = lastDayOfCurrentMonth.toISOString().split('T')[0];
+
+    // Parámetros para la solicitud
+    const params = {
+        "updated_timestamp__gte": startDate,
+        "updated_timestamp__lte": endDate,
+        "frequency": "month",        
+    };
+
+    axios.post('/business-gestion/dashboard/shop-product-sells-count/', params)
+        .then(response => {
+            // Obtener el valor de ventas de la respuesta
+            
+            const sellCount =  response.data.result[0] ? response.data.result[0].total : 0;
+            // Modificar el contenido del small-box con el valor de las ventas
+            const smallBox = document.getElementById('sellmount');
+            if (smallBox) {
+                smallBox.textContent = sellCount + " Ventas";
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+
 
 
 
