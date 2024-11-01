0818e64e3893f2a259d2d536ac9d7550ad6f0e7a
Enrique A. <ealmeida.cu@gmail.com>
2024-11-01 18:44:36 -0400

    rest of the ordering complete

--- apps/business_app/views/sell.py
+++ apps/business_app/views/sell.py
@@ -8,6 +8,8 @@ from apps.business_app.serializers.product import (
     ProductSerializer,
     ReadProductSerializer,
 )
+from django.db.models import Count, Sum, F
+
 from apps.business_app.serializers.sell import SellSerializer
 
 from apps.common.common_ordering_filter import CommonOrderingFilter
@@ -18,7 +20,12 @@ from apps.users_app.models.system_user import SystemUser
 
 
 class SellViewSet(viewsets.ModelViewSet, GenericAPIView):
-    queryset = Sell.objects.all().select_related("shop_product", "seller")
+    queryset = (
+        Sell.objects.all()
+        .select_related("shop_product", "seller")
+        .annotate(total_priced=F("quantity") * F("shop_product__sell_price"))
+        .annotate(profits=(F("shop_product__sell_price")-F("shop_product__cost_price")) * F("quantity")) 
+    )
     serializer_class = SellSerializer
     filter_backends = [
         DjangoFilterBackend,
