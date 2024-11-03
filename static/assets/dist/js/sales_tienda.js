9598add17b25a127fb95a95f899314392cafa333
Duniesky <dunieskydp86@gmail.com>
2024-11-03 15:32:49 -0500

    fix order sale tienda

--- static/assets/dist/js/sales_tienda.js
+++ static/assets/dist/js/sales_tienda.js
@@ -13,84 +13,85 @@ $(function () {
 });
 
 $(document).ready(function () {
-  
   const table = $("#tabla-de-Datos").DataTable({
-      responsive: true,
-      dom: '<"top"l>Bfrtip',
-      buttons: [
-        {
-          extend: "excel",
-          text: "Excel",
-        },
-        {
-          extend: "pdf",
-          text: "PDF",
-        },
-        {
-          extend: "print",
-          text: "Print",
-        },
-      ],
-      //Adding server-side processing
-      serverSide: true,
-      search: {
-        return: true,
+    responsive: true,
+    dom: '<"top"l>Bfrtip',
+    buttons: [
+      {
+        extend: "excel",
+        text: "Excel",
       },
-      processing: true,
-      ajax: function (data, callback, settings) {
-        const filters = $("#filter-form").serializeArray();
-        const params = {};
-        filters.forEach((filter) => {
-          if (filter.value) {
-            params[filter.name] = filter.value;
-          }
-        });
+      {
+        extend: "pdf",
+        text: "PDF",
+      },
+      {
+        extend: "print",
+        text: "Print",
+      },
+    ],
+    //Adding server-side processing
+    serverSide: true,
+    search: {
+      return: true,
+    },
+    processing: true,
+    ajax: function (data, callback, settings) {
+      const filters = $("#filter-form").serializeArray();
+      const params = {};
+      filters.forEach((filter) => {
+        if (filter.value) {
+          params[filter.name] = filter.value;
+        }
+      });
+      dir = "";      
+      if (data.order[0].dir == "desc") {
+        dir = "-";
+      }
       params.page_size = data.length;
       params.page = data.start / data.length + 1;
-      params.ordering = data.columns[data.order[0].column].data;
+      params.ordering = dir + data.columns[data.order[0].column].data;
       params.search = data.search.value;
-        dir = "";
-        if (data.order[0].dir == "desc") {
-          dir = "-";
-        }
+      
 
-        axios
-          .get(`${url}`, {params})
-          .then((res) => {
-            callback({
-              recordsTotal: res.data.count,
-              recordsFiltered: res.data.count,
-              data: res.data.results,
-            });
-          })
-          .catch((error) => {
-            alert(error);
+      axios
+        .get(`${url}`, { params })
+        .then((res) => {
+          callback({
+            recordsTotal: res.data.count,
+            recordsFiltered: res.data.count,
+            data: res.data.results,
           });
-      },
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    },
 
-      columns: [
-        { data: "shop_product__product__name", title: "Producto" },
-        { data: "quantity", title: "Cantidad" },
-        { data: "shop_product__sell_price", title: "Precio unitario" },
-        { data: "total_priced", title: "Monto total" },
-        { data: "seller__first_name", title: "Vendedor" },
-        { data: "created_timestamp", title: "Fecha" },
-        {
-          data: "id",
-          title: "Acciones",
-          render: (data, type, row) => {
-            return `<button type="button" title="delete" class="btn bg-olive" onclick="function_delete('${row.id}','${row.shop_product__product__name}','${row.quantity}','${row.created_timestamp}','${row.seller__first_name}')" >
+    columns: [
+      { data: "shop_product__product__name", title: "Producto" },
+      { data: "quantity", title: "Cantidad" },
+      { data: "shop_product__sell_price", title: "Precio unitario" },
+      { data: "total_priced", title: "Monto total" },
+      { data: "seller__first_name", title: "Vendedor" },
+      { data: "created_timestamp", title: "Fecha" },
+      {
+        data: "id",
+        title: "Acciones",
+        render: (data, type, row) => {
+          return `<button type="button" title="delete" class="btn bg-olive" onclick="function_delete('${row.id}','${row.shop_product__product__name}','${row.quantity}','${row.created_timestamp}','${row.seller__first_name}')" >
                           <i class="fas fa-trash"></i>
                         </button>                                          
                       </div>`;
-          },
         },
-      ],
-      //  esto es para truncar el texto de las celdas
-      columnDefs: [],
-    });
+      },
+    ],
+    order: [[6, 'desc']],
+    //  esto es para truncar el texto de las celdas
+    columnDefs: [],
+  });
 
-    // Manejo del formulario de filtros
+  // Manejo del formulario de filtros
   $("#filter-form").on("submit", function (event) {
     event.preventDefault();
     console.log("✌️event --->", event);
@@ -105,8 +106,8 @@ $(document).ready(function () {
 
   // Mostrar/Ocultar filtros
   $("#toggle-filters").on("click", function () {
-console.log('✌️function --->');
-    
+    console.log("✌️function --->");
+
     $("#filter-section").toggle();
   });
 });
