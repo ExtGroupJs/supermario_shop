d5fc62522ba6b61717845ca7adee6d4160f7a24e
Drak <ealmeida.cu@gmail.com>
2024-11-19 12:56:07 -0500

    Created model SellGroup

--- apps/business_app/models/__init__.py
+++ apps/business_app/models/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["Product", "Shop", "ShopProducts", "Sell"]
+__all__ = ["Product", "Shop", "ShopProducts", "Sell", "SellGroup"]
 from .product import Product
 from .shop import Shop
 from .shop_products import ShopProducts
 from .sell import Sell
+from .sell_group import SellGroup
