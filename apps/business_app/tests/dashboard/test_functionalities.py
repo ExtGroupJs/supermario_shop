37a05bb375ad6f4d79f7d9af686fb30075f349c6
Enrique A. <ealmeida.cu@gmail.com>
2024-11-26 02:34:03 -0500

    updated code

--- apps/business_app/tests/dashboard/test_functionalities.py
+++ apps/business_app/tests/dashboard/test_functionalities.py
@@ -24,9 +24,8 @@ class TestDashboardViewSetFunctionalities(BaseTestClass):
         self.client.force_authenticate(self.user)
 
     def test_shop_product_investment(self):
-        shops = [baker.make(Shop), 
-        baker.make(Shop)]
-        
+        shops = [baker.make(Shop), baker.make(Shop)]
+
         shop_products_per_shop = baker.random_gen.gen_integer(min_int=1, max_int=10)
         random_equal_cost = baker.random_gen.gen_integer(min_int=10, max_int=20)
         for shop in shops:
@@ -37,26 +36,75 @@ class TestDashboardViewSetFunctionalities(BaseTestClass):
                 quantity=1,
                 _quantity=shop_products_per_shop,
             )
-        
 
         url = reverse("dashboard-shop-product-investment")
         response = self.client.post(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
             response.data.get("investments"),
-            shop_products_per_shop * random_equal_cost * len(shops)
-            
+            shop_products_per_shop * random_equal_cost * len(shops),
         )
 
         # Testing filter by shop
-        for shop in shops: 
-            payload = {
-                "shop": shop.id
-            }
+        for shop in shops:
+            payload = {"shop": shop.id}
             response = self.client.post(url, data=payload, format="json")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
             self.assertEqual(
                 response.data.get("investments"),
-                shop_products_per_shop * random_equal_cost
-                
+                shop_products_per_shop * random_equal_cost,
             )
+
+    def test_sell_profits(self):
+        sell_group = baker.make(SellGroup)  # Initialy without any discount
+
+        shop_products_sold_qty = baker.random_gen.gen_integer(min_int=1, max_int=10)
+        random_equal_cost_price = baker.random_gen.gen_integer(min_int=1, max_int=10)
+        random_equal_sell_price = baker.random_gen.gen_integer(min_int=10, max_int=20)
+
+        for _ in range(shop_products_sold_qty):
+            baker.make(
+                Sell,
+                sell_group=sell_group,
+                shop_product=baker.make(
+                    ShopProducts,
+                    cost_price=random_equal_cost_price,
+                    sell_price=random_equal_sell_price,
+                    quantity=1,
+                ),
+                quantity=1,
+            )
+
+        url = reverse("dashboard-sell-profits")
+        response = self.client.post(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data.get("result"),
+            {
+                "frequency": "None",  # No frequency was specified
+                "total": shop_products_sold_qty
+                * (random_equal_sell_price - random_equal_cost_price),
+            },
+        )
+        self.assertEqual(response.data.get("discounts"), 0)
+
+        # Considering the discount
+        sell_group.discount = baker.random_gen.gen_integer(
+            min_int=1, max_int=random_equal_cost_price
+        )
+        sell_group.save()
+        response = self.client.post(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data.get("result"),
+            {
+                "frequency": "None",  # No frequency was specified
+                "total": shop_products_sold_qty
+                * (random_equal_sell_price - random_equal_cost_price),
+            },
+        )
+        self.assertEqual(response.data.get("discounts"), sell_group.discount)
+        self.assertEqual(
+            response.data.get("sell_group_ids"),
+            [sell_group.id],  # Esto solo viene si el discounts vino diferente de 0
+        )
--- apps/business_app/views/dashboard.py
+++ apps/business_app/views/dashboard.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
 from apps.business_app.models.sell import Sell
+from apps.business_app.models.sell_group import SellGroup
 from apps.business_app.models.shop_products import ShopProducts
 from apps.business_app.serializers.dashboard import (
     DashboardCountsSerializer,
@@ -71,10 +72,14 @@ class DashboardViewSet(
         serializer = self.get_serializer_class()(data=request.data)
         serializer.is_valid(raise_exception=True)
         frequency = serializer.validated_data.pop("frequency", None)
-        objects = Sell.objects.filter(**serializer.validated_data)
+        sell_objects = Sell.objects.filter(**serializer.validated_data)
+        sell_group_ids = sell_objects.values_list("sell_group_id", flat=True).distinct()
+        sell_group_discounts = SellGroup.objects.filter(
+            id__in=sell_group_ids
+        ).aggregate(Sum(F("discount")))
         if frequency:
             results = (
-                objects.annotate(
+                sell_objects.annotate(
                     frequency=self._get_frequency_function_given_payload_string(
                         frequency
                     )("updated_timestamp")
@@ -95,7 +100,7 @@ class DashboardViewSet(
                 .order_by("frequency")
             )
         else:
-            tmp_queryset = objects.annotate(
+            tmp_queryset = sell_objects.annotate(
                 total=Sum(
                     ExpressionWrapper(
                         (F("shop_product__sell_price") - F("shop_product__cost_price"))
@@ -104,13 +109,16 @@ class DashboardViewSet(
                     )
                 )
             ).values("total")
-            print(tmp_queryset)
             results = {
                 "frequency": "None",
                 "total": sum(item["total"] for item in tmp_queryset),
             }
+        result = {"result": results}
+        result["discounts"] = sell_group_discounts.get("discount__sum", 0)
+        if result["discounts"]:
+            result["sell_group_ids"] = list(sell_group_ids)
 
-        return Response({"result": results})
+        return Response(result)
 
     @action(
         detail=False,
