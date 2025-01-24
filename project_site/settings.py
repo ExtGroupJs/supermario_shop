98d34caa17c3b51ca78af10e2cc0a2811048d7b8
Drak <ealmeida.cu@gmail.com>
2025-01-24 09:57:00 -0500

    removing unnecesary lines

--- project_site/settings.py
+++ project_site/settings.py
@@ -23,8 +23,7 @@ import sentry_sdk
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
 
 STATIC_URL = "/static_output/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static_output/")
