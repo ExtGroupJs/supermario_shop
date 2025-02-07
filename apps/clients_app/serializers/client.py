676c4fa26cfff28722e9399968979bd90c93b763
ealmeida <ealmeida.cu@gmail.com>
2025-02-06 22:59:21 -0500

    added model to client serializers

--- apps/clients_app/serializers/client.py
+++ apps/clients_app/serializers/client.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
+from apps.business_app.serializers.model import ModelSerializer
 from apps.clients_app.models.client import Client
 
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ["id", "name", "phone", "shop"]
+        fields = ["id", "name", "phone", "shop", "models"]
+class ClientReadSerializer(ClientSerializer):
+    models = ModelSerializer(many = True)
+    class Meta(ClientSerializer.Meta):
+        fields = ["id", "name", "phone", "shop", "models"]
--- apps/clients_app/views/client.py
+++ apps/clients_app/views/client.py
@@ -6,8 +6,9 @@ from rest_framework import filters, permissions, viewsets
 from rest_framework.generics import GenericAPIView
 
 from apps.clients_app.models.client import Client
-from apps.clients_app.serializers.client import ClientSerializer
+from apps.clients_app.serializers.client import ClientReadSerializer, ClientSerializer
 from apps.common.common_ordering_filter import CommonOrderingFilter
+from apps.common.mixins.serializer_map import SerializerMapMixin
 from apps.users_app.serializers.group import GroupSerializer
 
 logger = logging.getLogger(__name__)
@@ -15,13 +16,15 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 
 
-class ClientViewSet(viewsets.ModelViewSet, GenericAPIView):
+class ClientViewSet(SerializerMapMixin, viewsets.ModelViewSet, GenericAPIView):
     """
     API endpoint that allows clients to be viewed or edited.
     """
 
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    list_serializer_class = ClientReadSerializer
+
 
     search_fields = [
         "name",
