16b24be48d7e616b6450b939896a2d06a3e25f6e
Enrique Almeida Maldonado <ealmeida.cu@gmail.com>
2025-04-15 00:46:53 -0400

    193 integration with drf orjson renderer to improve performance (#194)
    * first integration
    
    * using default parser

--- project_site/settings.py
+++ project_site/settings.py
@@ -246,6 +246,10 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_RENDERER_CLASSES": (
+        "drf_orjson_renderer.renderers.ORJSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ),
 }
 
 
