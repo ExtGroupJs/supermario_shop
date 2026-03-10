370fec4e2f737c6365df879bb1c6d0801c5352d5
duniesky <dunieskydp86@gmail.com>
2026-03-10 12:32:34 -0400

    cambios en tipo de tienda

--- apps/business_app/models/shop.py
+++ apps/business_app/models/shop.py
@@ -3,10 +3,29 @@ from django.db import models
 
 class Shop(models.Model):
     WHOLESALE_SHOP_NAME = "Tienda al por mayor"
+    TYPE_MECHANIC = "mecanica"
+    TYPE_TECHNOLOGY = "tecnologia"
+    TYPE_FOOD = "alimentos"
+    TYPE_OTHER = "otros"
+    TYPE_CHOICES = (
+        (TYPE_MECHANIC, "Mecanica"),
+        (TYPE_TECHNOLOGY, "Tecnologia"),
+        (TYPE_FOOD, "Alimentos"),
+        (TYPE_OTHER, "Otros"),
+    )
+
     name = models.CharField(verbose_name="Nombre", unique=True, max_length=200)
     logo = models.ImageField(verbose_name="logo")
     extra_info = models.TextField(
         verbose_name="Información Extra", null=True, blank=True
+    )
+    type = models.CharField(
+        verbose_name="Tipo",
+        max_length=20,
+        choices=TYPE_CHOICES,
+        null=True,
+        blank=True,
+        default=None,
     )
     products = models.ManyToManyField(
         to="business_app.Product", through="business_app.ShopProducts"
--- apps/business_app/serializers/shop.py
+++ apps/business_app/serializers/shop.py
@@ -6,4 +6,4 @@ from apps.business_app.models.shop import Shop
 class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
-        fields = ["id", "name", "logo", "extra_info", "enabled"]
+        fields = ["id", "name", "logo", "extra_info", "type", "enabled"]
